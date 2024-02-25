3737941d3d9c8ee82f2855bc301986e31f916ec0
davesalvador <101625754+davesalvador@users.noreply.github.com>
2024-02-25 13:23:03 +0800

    Creating

--- components/ContactCode.jsx
+++ components/ContactCode.jsx
@@ -1,55 +1,55 @@
-import styles from '../styles/ContactCode.module.css';
+import styles from "../styles/ContactCode.module.css";
 
 const contactItems = [
   {
-    social: 'website',
-    link: 'nitinranganath.me',
-    href: 'https://nitinranganath.me',
+    social: "website",
+    link: "nitinranganath.me",
+    href: "https://nitinranganath.me",
   },
   {
-    social: 'email',
-    link: 'nitinranganath@gmail.com',
-    href: 'mailto:nitinranganath@gmail.com',
+    social: "email",
+    link: "dave.salvador.dev@gmail.com",
+    href: "mailto:dave.salvador.dev@gmail.com",
   },
   {
-    social: 'github',
-    link: 'itsnitinr',
-    href: 'https://github.com/itsnitinr',
+    social: "github",
+    link: "davesalvador",
+    href: "https://github.com/davesalvador",
   },
   {
-    social: 'linkedin',
-    link: 'nitinranganath',
-    href: 'https://www.linkedin.com/in/nitinranganath/',
+    social: "linkedin",
+    link: "davesalvador",
+    href: "https://www.linkedin.com/in/dave-salvador-a388b9294/",
   },
   {
-    social: 'twitter',
-    link: 'iamnitinr',
-    href: 'https://www.twitter.com/iamnitinr',
+    social: "twitter",
+    link: "constdavejs",
+    href: "https://twitter.com/constdavejs",
   },
   {
-    social: 'instagram',
-    link: 'iamnitinr',
-    href: 'https://www.instagram.com/iamnitinr',
+    social: "instagram",
+    link: "iamnitinr",
+    href: "https://www.instagram.com/iamnitinr",
   },
   {
-    social: 'polywork',
-    link: 'nitinranganath',
-    href: 'https://www.polywork.com/nitinranganath',
+    social: "polywork",
+    link: "nitinranganath",
+    href: "https://www.polywork.com/nitinranganath",
   },
   {
-    social: 'telegram',
-    link: 'iamnitinr',
-    href: 'https://t.me/iamnitinr',
+    social: "telegram",
+    link: "iamnitinr",
+    href: "https://t.me/iamnitinr",
   },
   {
-    social: 'codepen',
-    link: 'nitinranganath',
-    href: 'https://codepen.io/itsnitinr',
+    social: "codepen",
+    link: "nitinranganath",
+    href: "https://codepen.io/itsnitinr",
   },
   {
-    social: 'codesandbox',
-    link: 'itsnitinr',
-    href: 'https://codesandbox.io/u/itsnitinr',
+    social: "codesandbox",
+    link: "itsnitinr",
+    href: "https://codesandbox.io/u/itsnitinr",
   },
 ];
 
@@ -61,7 +61,7 @@ const ContactCode = () => {
       </p>
       {contactItems.slice(0, 8).map((item, index) => (
         <p className={styles.line} key={index}>
-          &nbsp;&nbsp;&nbsp;{item.social}:{' '}
+          &nbsp;&nbsp;&nbsp;{item.social}:{" "}
           <a href={item.href} target="_blank" rel="noopener">
             {item.link}
           </a>
@@ -70,7 +70,7 @@ const ContactCode = () => {
       ))}
       {contactItems.slice(8, contactItems.length).map((item, index) => (
         <p className={styles.line} key={index}>
-          &nbsp;&nbsp;{item.social}:{' '}
+          &nbsp;&nbsp;{item.social}:{" "}
           <a href={item.href} target="_blank" rel="noopener">
             {item.link}
           </a>
